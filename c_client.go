8831473c56ec0a965131b08a25b07a0ad1e88753
Alex Shvid <a@shvid.com>
2023-04-06 21:30:42 -0700

    client

--- c_client.go
+++ c_client.go
@@ -27,67 +27,35 @@ type Instance struct {
 	Client  recordbase.Client
 }
 
-func (t *Instance) Get(tenant, primaryKey string) *GetBuilder {
-	return &GetBuilder {
-		Instance: t,
-		Request:  &recordpb.GetRequest {
-			Tenant: tenant,
-			PrimaryKey: primaryKey,
-		},
-	}
-}
-
 func (t *Instance) Close() {
 	t.Client.Destroy()
 }
 
-type InstanceBuilder struct {
-	Endpoint string
-	AuthToken string
-	UseTls bool
-	TimeoutMillis int
-}
+func Connect(endpoint, token string, tls bool, timeoutMillis int) (*Instance, error) {
 
-func (t *InstanceBuilder) Token(token string) *InstanceBuilder {
-	t.AuthToken = token
-	return t
-}
-
-func (t *InstanceBuilder) Tls(tls bool) *InstanceBuilder {
-	t.UseTls = tls
-	return t
-}
-
-func (t *InstanceBuilder) Timeout(timeoutMillis int) *InstanceBuilder {
-	t.TimeoutMillis = timeoutMillis
-	return t
-}
-
-func (t *InstanceBuilder) Connect() (*Instance, error) {
-
-	if t.TimeoutMillis > 0 {
-		clientDeadline := time.Now().Add(time.Duration(t.TimeoutMillis) * time.Millisecond)
+	if timeoutMillis > 0 {
+		clientDeadline := time.Now().Add(time.Duration(timeoutMillis) * time.Millisecond)
 		ctx, cancel := context.WithDeadline(context.Background(), clientDeadline)
 		defer cancel()
 
-		return t.doConnect(ctx)
+		return doConnect(ctx, endpoint, token, tls)
 	} else {
-		return t.doConnect(context.Background())
+		return doConnect(context.Background(), endpoint, token, tls)
 	}
 
 }
 
-func (t *InstanceBuilder) doConnect(ctx context.Context) (*Instance, error) {
+func doConnect(ctx context.Context, endpoint, token string, useTLS bool) (*Instance, error) {
 
 	var tlsConfig *tls.Config
-	if t.UseTls {
+	if useTLS {
 		tlsConfig = &tls.Config{
 			InsecureSkipVerify: true,
 			Rand:               rand.Reader,
 		}
 	}
 
-	client, err :=  recordbase.NewClient(ctx, t.Endpoint, t.AuthToken, tlsConfig)
+	client, err :=  recordbase.NewClient(ctx, endpoint, token, tlsConfig)
 	if err != nil {
 		return nil, err
 	}
@@ -97,62 +65,86 @@ func (t *InstanceBuilder) doConnect(ctx context.Context) (*Instance, error) {
 	}, nil
 }
 
-func New(commaSeparatedEndpoints string) *InstanceBuilder {
-	return &InstanceBuilder {
-		Endpoint: commaSeparatedEndpoints,
-	}
-}
-
 type Entry struct {
-	Columns  map[string][]byte
+	Tenant     string
+	PrimaryKey string
+	Version    int64
+	CreatedAt  int64
+	UpdatedAt  int64
+	DeletedAt  int64
+	Attributes map[string]string
+	Tags       []string
+	Columns    map[string][]byte
+	Files      map[string]*FileEntry
 }
 
-type GetBuilder struct {
-	Instance *Instance
-	Request  *recordpb.GetRequest
-	TimeoutMillis int
+type FileEntry struct {
+	Name      string
+	Data      []byte
+	Size      int32
+	CreatedAt int64
 }
 
-func (t *GetBuilder) FileContents(include bool) *GetBuilder {
-	t.Request.FileContents = include
-	return t
-}
+func (t *Instance) Get(tenant, primaryKey string, fileContents bool, timeoutMillis int) (*Entry, error) {
 
-func (t *GetBuilder) Timeout(timeoutMillis int) *GetBuilder {
-	t.TimeoutMillis = timeoutMillis
-	return t
-}
+	req := &recordpb.GetRequest {
+		Tenant:       tenant,
+		PrimaryKey:   primaryKey,
+		FileContents: fileContents,
+	}
 
-func (t *GetBuilder) ToEntry() (*Entry, error) {
+	if timeoutMillis > 0 {
 
-	if t.TimeoutMillis > 0 {
-
-		clientDeadline := time.Now().Add(time.Duration(t.TimeoutMillis) * time.Millisecond)
+		clientDeadline := time.Now().Add(time.Duration(timeoutMillis) * time.Millisecond)
 		ctx, cancel := context.WithDeadline(context.Background(), clientDeadline)
 		defer cancel()
 
-		return t.doToEntry(ctx)
+		return t.doGet(ctx, req)
 
 	} else {
-		return t.doToEntry(context.Background())
+		return t.doGet(context.Background(), req)
 	}
 
 }
 
-func (t *GetBuilder) doToEntry(ctx context.Context) (*Entry, error) {
+func (t *Instance) doGet(ctx context.Context, req *recordpb.GetRequest) (*Entry, error) {
 
-	resp, err := t.Instance.Client.Get(ctx, t.Request)
+	resp, err := t.Client.Get(ctx, req)
 	if err != nil {
 		return nil, err
 	}
 
-	m := make(map[string][]byte)
+	attrs := make(map[string]string)
+	for _, entry := range resp.Attributes {
+		attrs[entry.Name] = entry.Value
+	}
+
+	columns := make(map[string][]byte)
 	for _, entry := range resp.Columns {
-		m[entry.Name] = entry.Value
+		columns[entry.Name] = entry.Value
+	}
+
+	files := make(map[string]*FileEntry)
+	for _, entry := range resp.Files {
+		files[entry.Name] = &FileEntry{
+			Name:      entry.Name,
+			Data:      entry.Data,
+			Size:      entry.Size,
+			CreatedAt: entry.CreatedAt,
+		}
 	}
 
 	return &Entry {
-		Columns:  m,
+		Tenant: resp.Tenant,
+		PrimaryKey: resp.PrimaryKey,
+		Version: resp.Version,
+		CreatedAt: resp.CreatedAt,
+		UpdatedAt: resp.UpdatedAt,
+		DeletedAt: resp.DeletedAt,
+		Attributes: attrs,
+		Tags: resp.Tags,
+		Columns:  columns,
+		Files: files,
 	}, nil
 
 }
